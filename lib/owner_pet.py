fd9299ee7dc555733dac589e9c3a9c954a4ed0a0
Ian Tuitoek <iantuitoek4@gmail.com>
2025-10-17 16:00:43 +0300

    Refactor Pet and Owner classes for improved readability and consistency; update error messages for clarity.

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,22 +1,24 @@
 class Pet:
-    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+
+    PET_TYPES = ['dog', 'cat', 'rodent', 'bird', 'reptile', 'exotic']
     all = []
-    def __init__(self,name,pet_type,owner=None):
+
+    def __init__(self, name, pet_type, owner=None):
         self.name = name
         self.pet_type = pet_type
         self.owner = owner
         Pet.all.append(self)
-    
+
     @property
     def pet_type(self):
         return self._pet_type
-    
+
     @pet_type.setter
-    def pet_type(self,pet_type):
+    def pet_type(self, pet_type):
         if pet_type not in self.PET_TYPES:
-            raise Exception('Not a valid pet type')
+            raise Exception('Not a valid pet type.')
         self._pet_type = pet_type
-    
+
     @property
     def owner(self):
         return self._owner
@@ -27,16 +29,19 @@ class Pet:
             raise Exception("Object is not of type Owner")
         self._owner = owner
 
+
+
 class Owner:
-    def __init__(self,name):
+    def __init__(self, name):
         self.name = name
 
     def pets(self):
         return [pet for pet in Pet.all if pet.owner == self]
-    def add_pet(self,pet):
-        if not isinstance(pet,Pet):
-            raise Exception('Input is not of type Pet')
-        pet.owner = self
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("Input object is not of type Pet")
+        pet.owner = self            
 
     def get_sorted_pets(self):
-        return sorted(self.pets(),key=lambda pet: pet.name)
+        return sorted(self.pets(), key=lambda pet: pet.name)
